a8e06ffe02b31a959a76970470a7f37ab34e43cb
Mahudoom <66937573+easa2000@users.noreply.github.com>
2021-07-02 17:56:14 +0530

    Create park ticket.c

---
+++ park ticket.c
@@ -0,0 +1,31 @@
+//#include<stdio.h>
+//void main(){
+//	char ch;
+//	int rate,hrs,ehr,emin,lhr,lmin;
+//	printf("\n * Truck/Bus - 20 Rs per hour");
+//	printf("\n * Car - 10 Rs per hour");
+//	printf("\n * Scooter/Cycle/Motor cycle - 5 rs per hour");
+//	printf("\n Enter the first letter of choose (eg: Car as 'c'): ");
+//	scanf("%c",&ch);
+//	printf("\n Enter hour of entered ");
+//	scanf("%d",&ehr);
+//	printf("\n Enter minute of entered ");
+//	scanf("%d",&emin);
+//	
+//	printf("\n Enter hour of leaved ");
+//	scanf("%d",&lhr);
+//	printf("\n Enter minute of leaved ");
+//	scanf("%d",&lmin);
+//	
+//	hrs = lhr - ehr;
+//	
+//	
+//	if(ch == 't' || ch == 'b'){
+//		rate = 20 * hrs;
+//	}else if(ch=='c'){
+//		rate = 10 * hrs;
+//	}else{
+//		rate = 5 * hrs;
+//	}
+//	printf("\n Rate of parking : %d",rate);
+//}
